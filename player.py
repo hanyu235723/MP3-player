b12cb800843f01273157b2c5a653a41273ccfb6e
hanyu235723 <hanyuyu2018@gmail.com>
2019-05-02 11:17:52 -0700

    add import from file and from dir func add save to db and load from db for local play list

--- player.py
+++ player.py
@@ -1,6 +1,7 @@
 
 import pygame, tkinter as tk
 from tkinter.font import Font
+from tkinter import filedialog
 from mutagen.mp3 import MP3
 import eyed3
 from tkinter import ttk
@@ -11,11 +12,16 @@ from os.path import isfile, join
 from random import choice
 from threading import Thread
 import sys, re
+from sqlalchemy import create_engine,MetaData
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column,String,INTEGER,REAL,TEXT
+from sqlalchemy.orm import sessionmaker
 
 global path, check_event_thread, lyric, driver, line_index, display_flag, image,start_index,end_index
 song_path = '/home/yu/Downloads/Gmail'
 lyricpath = '/home/yu/Music/lyric'
 imagepath = '/home/yu/Pictures'
+default_song= "/home/yu/Downloads/Gmail/I Can't Change.mp3"
 isPaused =False
 global line_index,sequence_mode
 sequence_mode=0
@@ -31,6 +37,47 @@ QUIT_EVENT = pygame.USEREVENT + 1
 pygame.mixer_music.set_endevent(SONG_END_EVENT)
 quit_event = pygame.event.Event(QUIT_EVENT, message="stop thread")
 
+eng= create_engine('sqlite:///song.db')
+base = declarative_base()
+metadata= MetaData()
+Session= sessionmaker(bind=eng)
+session= Session()
+
+class Song_DB(base):
+
+    __tablename__="Songs"
+    id= Column(INTEGER,primary_key=True,autoincrement=True)
+    title = Column(TEXT)
+    song_path = Column(TEXT)
+    lyric_file = Column(TEXT)
+    length= Column(REAL)
+    album=Column(TEXT)
+    artist=Column(TEXT)
+
+base.metadata.create_all(eng)
+
+def get_lyric(lyric_file):
+    global lyric_line_pattern, lyricpath
+    lyriclist = []
+
+    def time_to_mill_seconds(time):
+        sec = int(time[1:3]) * 60 + float(time[4:len(time) - 1])
+        return int(sec * 1000)
+
+    if not os.path.isfile(lyric_file):
+        return []
+    else:
+        with open(lyric_file) as f:
+            for line in f.readlines():
+                time = lyric_line_pattern.match(line)
+                if time:
+                    play_seconds = time_to_mill_seconds(time.group())
+
+                    lyriclist.append(
+                        [play_seconds, lyric_split_pattern.split(line, maxsplit=1)[1]]
+                    )
+    return lyriclist
+
 class Song():
     #paramete"title" should be abs path for song file
     def __init__(self,path):
@@ -39,6 +86,7 @@ class Song():
         self.song_path= path
         self.lyric_file = os.path.join(lyricpath,self.title+".lrc")
         self.length= MP3(path).info.length
+        self.lyric=get_lyric(self.lyric_file)
 
     @property
     def album(self):
@@ -46,6 +94,9 @@ class Song():
             return self.audiofile.tag.album
         except (KeyError):
             return "no album"
+    @album.setter
+    def album(self,albumname):
+        self._album=albumname
 
     @property
     def artist(self):
@@ -53,46 +104,32 @@ class Song():
             return self.audiofile.tag.artist
         except ( KeyError):
             return "no artist"
-   # get the lyric list :
-    # [time in millseconds][ lyric text ]
-    @property
-    def lyric(self):
-        global lyric_line_pattern,lyricpath
-        lyriclist = []
+    @artist.setter
+    def artist(self,artist):
+        self._artist=artist
 
-        def time_to_mill_seconds(time):
-            sec = int(time[1:3]) * 60 + float(time[4:len(time) - 1])
-            return int(sec * 1000)
-        if not os.path.isfile(self.lyric_file):
-            return []
-        else:
-            with open(self.lyric_file) as f:
-                for line in f.readlines():
-                    time = lyric_line_pattern.match(line)
-                    if time:
-                        play_seconds = time_to_mill_seconds(time.group())
-
-                        lyriclist.append(
-                            [play_seconds, lyric_split_pattern.split(line, maxsplit=1)[1]]
-                        )
-        return lyriclist
     def __repr__(self):
         return "{}'s artist :{} album : {}, len : {},lyric :{}".format(self.title,self.artist,self.album,self.length,self.lyric)
 
+def Save_2_DB(Song):
+    song_info= Song_DB(title=Song.title,song_path=Song.song_path,lyric_file=Song.lyric_file,length=Song.length,album=Song.album,artist=Song.artist)
+    session.add(song_info)
+    session.commit()
+
 class Application(tk.Frame):
     def __init__(self, master=None):
+
         super().__init__(master)
-        self.Song_list=[]
+        self.Local_Song_list=[]
+        self.trial_Song_list=[]
         self.Create_Song_List()
         self.song_index = 0
-        self.current_song = self.Song_list[self.song_index]
+        self.current_song = self.Local_Song_list[self.song_index]
         self.Create_UI()
         self.bold_font = Font(family="Helvetica", size=20, weight="bold")
         self.lyric.tag_configure("bold", font=self.bold_font, foreground="Blue")
-
         self.Play()
         self.Status_update()
-
 
     def Create_UI(self):
         global imagepath
@@ -100,20 +137,14 @@ class Application(tk.Frame):
         self.list = tk.Listbox(self, yscrollcommand=self.scroll_bar.set, bg="white",
                                selectbackground="grey")
         self.scroll_bar.config(command=self.list.yview)
-        index = 0
-        for item in self.Song_list:
-            self.list.insert(index, (item.title,item.artist,item.album))
-            index += 1
-
+        self.Refresh_SonglistUI()
         self.list.bind('<<ListboxSelect>>', self.Click_Song)
-        play_image = os.path.join(imagepath, 'play.png')
+        play_image = os.path.join(imagepath, 'play.jpeg')
     # due to above error ,so use image module PIL to open image to see if it works
         im = Image.open(play_image)
         width, height = im.size
         im.thumbnail((width / 4, height / 4))
-
         ph = ImageTk.PhotoImage(im)
-
         self.Pause_button = tk.Button(self, text="Pause&Resume", activebackground="white", command=self.pause_resume,image=ph)
         self.Pause_button.image = ph
 
@@ -122,19 +153,24 @@ class Application(tk.Frame):
         self.random_button = tk.Button(self, text="Random", activebackground="white", command=self.set_to_random_mode)
         self.sequence_button = tk.Button(self, text="Sequence", activebackground="white",
                                          command=self.set_to_sequence_mode)
-       # self.import_button = tk.Button(self,text="Import",activebackground= "white",command= self.Import)
+
+        self.import_single = tk.Button(self, text="import single song", activebackground="white",
+                                         command=self.Import_single_song)
+        self.import_dir =tk.Button(self,text="import from dir",activebackground="white",command=self.Import_from_dir)
         self.quit = tk.Button(self, text="QUIT", fg="red", activebackground="white",
                               command=self.Quit)
-        #        self.volumn_scale=tk.Scale(self,command=self.Process_Adjust,variable=vars)
+
         lyric_image = Image.open(imagepath+ "/lyric.jpg")
         lyric_ph = ImageTk.PhotoImage(lyric_image)
         self.lyric = tk.Text(self)
         self.progress_bar = ttk.Progressbar(mode="determinate", maximum=70000, value=0)
-        self.grid(row=0, column=0, rowspan=20, columnspan=15)
-        self.list.grid(row=0, column=1, rowspan=15, columnspan=10, sticky=tk.N + tk.S + tk.W, padx=2, pady=2)
+
+        self.grid(row=0,column=0,rowspan = 20,columnspan= 20)
+        self.list.grid(row=0, column=1, rowspan=15, columnspan=15,padx=2, pady=2)
+     #   self.list.grid(row=0, column=1, rowspan=15, columnspan=15,sticky=tk.N + tk.S + tk.W, padx=2, pady=2)
+        self.lyric.grid(row=0, column=21, columnspan=8, rowspan=20, padx=3, pady=3)
         self.scroll_bar.grid(row=0, column=0, rowspan=15)
-        self.progress_bar.grid(row=16, column=0, columnspan=5, sticky=tk.N + tk.S)
-        # self.play_button.grid(row=18, column=1)
+        self.progress_bar.grid(row=16, column=0, columnspan=15, sticky=tk.N + tk.S)
         self.Pause_button.grid(row=18, column=2)
         self.next_button.grid(row=18, column=3)
         self.last_button.grid(row=18, column=4)
@@ -142,19 +178,59 @@ class Application(tk.Frame):
         self.sequence_button.grid(row=18, column=6)
 #        self.import_button.grid(row=19,column=2)
         self.quit.grid(row=18, column=7)
-        self.lyric.grid(row=0, column=8, columnspan=8, rowspan=20, padx=3, pady=3)
+        self.import_single.grid(row=19,column=1)
+        self.import_dir.grid(row=19,column=2)
+
+    def Refresh_SonglistUI(self):
+        index = 0
+        self.list.delete(0,tk.END)
+        for item in self.Local_Song_list:
+            self.list.insert(index, (item.title, item.artist, item.album))
+            index += 1
 
     def Create_Song_List(self):
-        global song_path
-        if not os.path.exists(song_path):
+        global session,default_song
+        for instance in session.query(Song_DB).order_by(Song_DB.title):
+            song = Song(path=(instance.song_path))
+            song.title = instance.title
+            song.album = instance.album
+            song.length = instance.length
+            song.lyric_file = instance.lyric_file
+            song.artist=instance.artist
+            song.lyric = get_lyric(song.lyric_file)
+            self.Local_Song_list.append(song)
+        if (len(self.Local_Song_list)==0):
+            self.Local_Song_list.append(Song(path=default_song))
+
+    def Import_single_song(self):
+        song_file = filedialog.askopenfilename(initialdir="/home/yu/Downloads/Gmail/",title= "select file",filetypes=[("mp3 file",'*.mp3'),("all file","*.*")])
+        if song_file :
+
+            for song in self.Local_Song_list:
+                if song.song_path==song_file:
+                    break
+            else:
+                new_song = Song(song_file)
+                self.Local_Song_list.append(new_song)
+                self.Local_Song_list.sort(key=lambda x: x.title)
+                index=self.Local_Song_list.index(new_song)
+                Save_2_DB(new_song)
+                self.list.insert(index, (new_song.title, new_song.artist, new_song.album))
+
+    def Import_from_dir(self):
+        song_dir = filedialog.askdirectory(initialdir="/home/yu/Downloads/Gmail/")
+        if not os.path.exists(song_dir):
             print("filepath not exists")
         else:
-            for f in os.listdir(song_path):
-                song_file = os.path.join(song_path, f)
+            for f in os.listdir(song_dir):
+                song_file = os.path.join(song_dir, f)
                 if os.path.isfile(song_file):
-                    self.Song_list.append(Song(song_file))
+                    new_song = Song(song_file)
+                    self.Local_Song_list.append(new_song)
+                    Save_2_DB(new_song)
 
-        self.Song_list.sort(key=lambda x:x.title)
+        self.Local_Song_list.sort(key=lambda x: x.title)
+        self.Refresh_SonglistUI()
 
     def Play(self):
         global isPaused
@@ -212,13 +288,13 @@ class Application(tk.Frame):
 
         if (sequence_mode == 0):
             # sequence play
-            if (self.song_index) < len(self.Song_list) - 1:
+            if (self.song_index) < len(self.Local_Song_list) - 1:
                 self.song_index += 1
             else:
                 self.song_index = 0
         else:
             self.song_index = self.random_select()
-        self.current_song=self.Song_list[self.song_index]
+        self.current_song=self.Local_Song_list[self.song_index]
 
     def Set_index2_Prev(self):
         self.list.select_clear(self.song_index, self.song_index)
@@ -226,11 +302,11 @@ class Application(tk.Frame):
             if (self.song_index) > 0:
                 self.song_index -= 1
             else:
-                self.song_index = len(self.Song_list) - 1
+                self.song_index = len(self.Local_Song_list) - 1
         else:
             self.song_index = self.random_select()
 
-        self.current_song = self.Song_list[self.song_index]
+        self.current_song = self.Local_Song_list[self.song_index]
     def Stop(self):
         global isPaused
         isPaused = True
@@ -250,10 +326,10 @@ class Application(tk.Frame):
         self.Status_update()
         self.Play()
     def random_select(self):
-        next_song = choice(self.Song_list)
-        while next_song == self.Song_list[self.song_index]:
-            next_song = choice(self.Song_list)
-        return self.Song_list.index(next_song)
+        next_song = choice(self.Local_Song_list)
+        while next_song == self.Local_Song_list[self.song_index]:
+            next_song = choice(self.Local_Song_list)
+        return self.Local_Song_list.index(next_song)
 
     def set_to_random_mode(self):
         global sequence_mode
@@ -277,15 +353,19 @@ class Application(tk.Frame):
         if (len(indexs) == 1):
             idx = indexs[0]
             self.song_index = idx
-            self.__Play()
+            self.current_song= self.Local_Song_list[idx]
+            self.Stop()
+            self.Status_update()
+            self.Play()
         else:
             print(indexs)
 
     def check_event(self):
-        global line_index,quit_event,SONG_END_EVENT
+        global line_index,quit_event,SONG_END_EVENT,session
         while True:
             for event in pygame.event.get():
                 if event == quit_event:
+                    session.close()
                     return 0
                 # get event if song is over and play next song automatically
                 if event.type == SONG_END_EVENT:
